0829de79d5cd31a1d20a6f8e21b3def6baf29c67
meryem.karakas@hmb.gov.tr <meryem.karakas@hmb.gov.tr>
2023-08-03 10:58:36 +0300

    3 Ağustos Profil Değişiklik

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect, useRef } from "react";
 import { useDropzone } from "react-dropzone";
 import { Button, Box, Typography } from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
@@ -6,11 +6,16 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
+import InputMask from "react-input-mask";
 import axios from "axios";
 
 const Profile = () => {
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [open, setOpen] = React.useState(false);
   const [cities, setCities] = useState([]);
-  const [selectedCity, setSelectedCity] = useState(null); // Initially, it's null until the user selects a city
+  const [selectedCity, setSelectedCity] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const inputMaskRef = useRef(null);
 
   const fetchDataFromAPI = async () => {
     try {
@@ -26,6 +31,17 @@ const Profile = () => {
     fetchDataFromAPI();
   }, []);
 
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    setUploadedFile(file);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: "image/jpeg, image/jpg, image/png",
+    multiple: false,
+    onDrop,
+  });
+
   const saveProfileData = async () => {
     try {
       if (!selectedCity) {
@@ -33,14 +49,15 @@ const Profile = () => {
         return;
       }
 
-      const requestData = {
-        city: selectedCity.cityName, // Use the cityName from the selectedCity object
-        cityId: selectedCity.id, // Use the id from the selectedCity object
-      };
+      const requestData = new FormData();
+      requestData.append("city", selectedCity.cityName);
+      requestData.append("cityId", selectedCity.id);
+      requestData.append("phoneNumber", phoneNumber);
+      requestData.append("profilePhoto", uploadedFile); // Add the uploaded photo to the formData
 
       await axios.post("http://localhost:8080/api/user/profile", requestData, {
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "multipart/form-data", // Set the correct Content-Type for formData
         },
       });
 
@@ -49,6 +66,23 @@ const Profile = () => {
       console.error("Error while saving profile data:", error);
     }
   };
+
+  const TextMaskCustom = React.forwardRef((props, ref) => {
+    const { mask, ...other } = props;
+
+    return (
+      <InputMask
+        {...other}
+        mask={mask}
+        guide={false}
+        placeholderChar={'\u2000'}
+        showMask
+        ref={ref}
+      />
+    );
+  });
+
+  const phoneMask = "(999) 999-9999";
 
   return (
     <div>
@@ -59,7 +93,36 @@ const Profile = () => {
       >
         PROFİLİM
       </Typography>
-
+      <Box
+        {...getRootProps()}
+        sx={{
+          width: "175px",
+          height: "175px",
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          cursor: "pointer",
+          marginLeft: "0",
+          marginRight: "auto",
+          border: "1px solid #ccc",
+          marginTop: "20px",
+        }}
+      >
+        <input {...getInputProps()} />
+        {uploadedFile ? (
+          <img
+            src={URL.createObjectURL(uploadedFile)}
+            alt="Uploaded"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        ) : (
+          <Typography variant="body1" color="textSecondary">
+            {isDragActive
+              ? "Dosyayı bırakın..."
+              : "Resmi sürükleyin veya tıklayın"}
+          </Typography>
+        )}
+      </Box>
       <Box
         sx={{
           display: "flex",
@@ -68,12 +131,20 @@ const Profile = () => {
           marginTop: "20px",
         }}
       >
-        <FormControl
-          sx={{ minWidth: 120, width: "250px", marginBottom: "10px" }}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setUploadedFile(null)}
+          style={{
+            width: "175px",
+            backgroundColor: "green",
+            marginBottom: "10px",
+          }}
         >
-          <InputLabel id="demo-simple-select-helper-label">
-            Yaşanılan Şehir
-          </InputLabel>
+          FOTOĞRAFI KALDIR
+        </Button>
+        <FormControl sx={{ minWidth: 120, width: "250px", marginBottom: "10px" }}>
+          <InputLabel id="demo-simple-select-helper-label">Yaşanılan Şehir</InputLabel>
           <Select
             labelId="demo-simple-select-helper-label"
             id="demo-simple-select-helper"
@@ -98,6 +169,18 @@ const Profile = () => {
             ))}
           </Select>
         </FormControl>
+        <TextField
+          label="Telefon Numarası"
+          variant="outlined"
+          InputProps={{
+            inputComponent: (props) => <TextMaskCustom {...props} mask={phoneMask} />,
+            inputProps: {
+              ref: inputMaskRef,
+              value: phoneNumber, // Set the value from the state
+              onChange: (e) => setPhoneNumber(e.target.value), // Handle changes and update state
+            },
+          }}
+        />
         <Button
           variant="contained"
           color="primary"
