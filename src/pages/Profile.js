8e88e68ecfa0c6329619091b223a610430ad8822
meryem.karakas@hmb.gov.tr <meryem.karakas@hmb.gov.tr>
2023-08-04 17:03:25 +0300

    4 Ağustos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,35 +1,13 @@
 import React, { useCallback, useState, useEffect, useRef } from "react";
 import { useDropzone } from "react-dropzone";
 import { Button, Box, Typography } from "@mui/material";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import TextField from "@mui/material/TextField";
-import InputMask from "react-input-mask";
 import axios from "axios";
 
 const Profile = () => {
   const [uploadedFile, setUploadedFile] = useState(null);
-  const [open, setOpen] = React.useState(false);
-  const [cities, setCities] = useState([]);
-  const [selectedCity, setSelectedCity] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const inputMaskRef = useRef(null);
+  
 
-  const fetchDataFromAPI = async () => {
-    try {
-      const response = await axios.get("http://localhost:8080/api/user/cities");
-      const data = response.data;
-      setCities(data);
-    } catch (error) {
-      console.error("Veri alınırken hata oluştu:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchDataFromAPI();
-  }, []);
+  
 
   const onDrop = useCallback((acceptedFiles) => {
     const file = acceptedFiles[0];
@@ -40,24 +18,17 @@ const Profile = () => {
     accept: "image/jpeg, image/jpg, image/png",
     multiple: false,
     onDrop,
-  });
+  }); 
 
   const saveProfileData = async () => {
     try {
-      if (!selectedCity) {
-        console.error("Şehir seçmediniz!");
-        return;
-      }
-
+      
       const requestData = new FormData();
-      requestData.append("city", selectedCity.cityName);
-      requestData.append("cityId", selectedCity.id);
-      requestData.append("phoneNumber", phoneNumber);
-      requestData.append("profilePhoto", uploadedFile); // Add the uploaded photo to the formData
+      requestData.append("profilePhoto", uploadedFile); 
 
       await axios.post("http://localhost:8080/api/user/profile", requestData, {
         headers: {
-          "Content-Type": "multipart/form-data", // Set the correct Content-Type for formData
+          "Content-Type": "multipart/form-data", 
         },
       });
 
@@ -66,23 +37,6 @@ const Profile = () => {
       console.error("Error while saving profile data:", error);
     }
   };
-
-  const TextMaskCustom = React.forwardRef((props, ref) => {
-    const { mask, ...other } = props;
-
-    return (
-      <InputMask
-        {...other}
-        mask={mask}
-        guide={false}
-        placeholderChar={'\u2000'}
-        showMask
-        ref={ref}
-      />
-    );
-  });
-
-  const phoneMask = "(999) 999-9999";
 
   return (
     <div>
@@ -143,44 +97,6 @@ const Profile = () => {
         >
           FOTOĞRAFI KALDIR
         </Button>
-        <FormControl sx={{ minWidth: 120, width: "250px", marginBottom: "10px" }}>
-          <InputLabel id="demo-simple-select-helper-label">Yaşanılan Şehir</InputLabel>
-          <Select
-            labelId="demo-simple-select-helper-label"
-            id="demo-simple-select-helper"
-            value={selectedCity}
-            label="Yaşanılan Şehir"
-            MenuProps={{
-              PaperProps: {
-                style: {
-                  maxHeight: 300,
-                },
-              },
-            }}
-            onChange={(e) => setSelectedCity(e.target.value)}
-          >
-            <MenuItem value="">
-              <em>Seçimi temizle</em>
-            </MenuItem>
-            {cities.map((city) => (
-              <MenuItem key={city.id} value={city}>
-                {city.cityName}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <TextField
-          label="Telefon Numarası"
-          variant="outlined"
-          InputProps={{
-            inputComponent: (props) => <TextMaskCustom {...props} mask={phoneMask} />,
-            inputProps: {
-              ref: inputMaskRef,
-              value: phoneNumber, // Set the value from the state
-              onChange: (e) => setPhoneNumber(e.target.value), // Handle changes and update state
-            },
-          }}
-        />
         <Button
           variant="contained"
           color="primary"
